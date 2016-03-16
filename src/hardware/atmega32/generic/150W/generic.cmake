4786ec8a8812e14328281eabf46de65fcef3e431
Vojtech Suk <vojta@nemilkov.cz>
2016-03-16 02:07:40 +0100

    Add support for GT Power A8

---
+++ src/hardware/atmega32/generic/150W/generic.cmake
@@ -0,0 +1,12 @@
+
+set(GENERIC_SOURCE
+    GTPowerA8.cpp
+    GTPowerA8.h
+    AnalogInputsADC.cpp
+    AnalogInputsADC.h
+
+    Hardware.h
+    HardwareConfigGeneric.h
+)
+
+CHEALI_ADD(GENERIC_SOURCE_FILES "${GENERIC_SOURCE}")
